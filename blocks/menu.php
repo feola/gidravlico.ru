86f357eb5abfce64b91b75e2d76ce8eefe195398
feola <feolan@mail.ru>
2012-11-28 22:18:08 +0400

    modified menu

--- blocks/menu.php
+++ blocks/menu.php
@@ -1,18 +1,18 @@
     <div id="menu">
       <div id="menu_list">
-        <a href="index.php">Главная</a>
+        <a href="../index/index.php">Главная</a>
         <img src="images/splitter.gif" class="splitter" alt="" />
-        <a href="theory.php">Теория</a>
+        <a href="../theory/theory.php">Теория</a>
         <img src="images/splitter.gif" class="splitter" alt="" />
-        <a href="practic.php">Практика</a>
+        <a href="../practic/practic.php">Практика</a>
         <img src="images/splitter.gif" class="splitter" alt="" />
-        <a href="video.php">Видео</a>
+        <a href="../video/video.php">Видео</a>
         <img src="images/splitter.gif" class="splitter" alt="" />
-        <a href="library.php">Библиотека</a>
+        <a href="../library/library.php">Библиотека</a>
         <img src="images/splitter.gif" class="splitter" alt="" />
-		<a href="download.php">Скачать</a>
+		<a href="../download/download.php">Скачать</a>
         <img src="images/splitter.gif" class="splitter" alt="" />
-        <a href="contacts.php">Контакты</a>
+        <a href="../contacts/contacts.php">Контакты</a>
       </div>
     </div>
 
