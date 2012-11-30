f7027a04fe137ac5ed771d3414752dae4ca43e3f
feola <feolan@mail.ru>
2012-11-30 21:46:36 +0400

    created vozn_i_str_turb

---
+++ content/video/vozn_i_str_turb.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+  <head>
+    <title>gidravlico.ru</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="../../style.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+
+<?php include ("../../blocks/menu.php");?>
+
+<?php include ("../../blocks/header.php");?>
+
+    <div id="content">
+
+      <h1>Возникновение и структура турбулентности</h1><br/>
+
+<p>
+  <object width="480" height="360">
+    <param name="movie"
+           value="http://www.youtube.com/v/3BXpUXpylwI?version=3&amp;autohide=1">
+</param>
+<param name="allowFullScreen" value="true">
+</param>
+<param name="allowscriptaccess" value="always">
+</param>
+<embed src="http://www.youtube.com/v/3BXpUXpylwI?version=3&amp;autohide=1"
+       type="application/x-shockwave-flash" allowscriptaccess="always"
+       allowfullscreen="true" width="480" height="360">
+</embed>
+  </object>
+</p>
+
+    </div>
+
+<?php include ("../../blocks/footer.php");?>
+
+  </body>
+
+</html>
