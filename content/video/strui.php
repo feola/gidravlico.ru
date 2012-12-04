626e1653c4a7d78ac4020eef5c60f789b495cd31
feola <feolan@mail.ru>
2012-12-04 14:52:01 +0400

    strui

---
+++ content/video/strui.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+  <head>
+    <title>gidravlico.ru</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="../../style.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+
+<?php include ("../../blocks/menu.php");?>
+
+<?php include ("../../blocks/header.php");?>
+
+    <div id="content">
+
+     <br/><center><h1>Струйные течения</h1><br/><br/></center>
+
+<center>
+<p>
+  <object width="480" height="360">
+    <param name="movie"
+           value="http://www.youtube.com/v/kVueME2E_1g?version=3&amp;autohide=1">
+</param>
+<param name="allowFullScreen" value="true">
+</param>
+<param name="allowscriptaccess" value="always">
+</param>
+<embed src="http://www.youtube.com/v/kVueME2E_1g?version=3&amp;autohide=1"
+       type="application/x-shockwave-flash" allowscriptaccess="always"
+       allowfullscreen="true" width="480" height="360">
+</embed>
+  </object>
+</p>
+
+<br/>
+
+      <p>По своим свойствам различают незатопленные и затопленные струйные течения.<br/>
+         Струя воды в воздушной среде - незатопленная. Та же струя в воде - затопленная.</p>
+
+</center>
+
+    </div>
+
+<?php include ("../../blocks/footer.php");?>
+
+  </body>
+
+</html>
