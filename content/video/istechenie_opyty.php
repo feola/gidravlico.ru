d7290646566ea957c56b2d4a4002709e62ce03e9
feola <feolan@mail.ru>
2012-12-04 14:05:06 +0400

    istechenie_opyty

---
+++ content/video/istechenie_opyty.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+  <head>
+    <title>gidravlico.ru</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="../../style.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+
+<?php include ("../../blocks/menu.php");?>
+
+<?php include ("../../blocks/header.php");?>
+
+    <div id="content">
+
+     <br/><center><h1>Демонстрация опытов с истечением жидкости</h1><br/><br/></center>
+
+<center>
+<p>
+  <object width="480" height="360">
+    <param name="movie"
+           value="http://www.youtube.com/v/TZSLQYoRmG4?version=3&amp;autohide=1">
+</param>
+<param name="allowFullScreen" value="true">
+</param>
+<param name="allowscriptaccess" value="always">
+</param>
+<embed src="http://www.youtube.com/v/TZSLQYoRmG4?version=3&amp;autohide=1"
+       type="application/x-shockwave-flash" allowscriptaccess="always"
+       allowfullscreen="true" width="480" height="360">
+</embed>
+  </object>
+</p>
+
+<br/>
+
+      <p>Для демонстрации опытов истечения жидкости применяется напорный бак,<br/>
+         имеющий приспособление для установки отверстий и насадков.</p>
+
+</center>
+
+    </div>
+
+<?php include ("../../blocks/footer.php");?>
+
+  </body>
+
+</html>
